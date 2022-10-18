3a63bf903c0fc289ca350c2c8c898d1a48510121
Emanuel Angelo <emanuelangelo@outlook.com.br>
2022-10-18 11:28:47 -0300

    rotas aninhadas

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="projetos">
-    <h1 class="title">Projetos</h1>
     <form @submit.prevent="salvar">
       <div class="field">
         <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
